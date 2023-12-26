61d64cb0e7cb8a7c7bc182380b27b0feceb10ab8
ArnavRupde <arnavrupade@gmail.com>
2023-12-26 12:24:25 +0530

    Wrap express app with websocker server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import express from 'express';
+import http from 'http';
+import {WebSocketServer} from 'ws';
 
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
 const port = 3000;
+
+wss.on("connection", async(ws, req)=> {
+    console.log("client connected");
+    ws.on("message", (message) => {
+        console.log(`Received message from client: ${message}`);
+        ws.send(`Original message was: ${message}`);
+    });
+});
 
 app.get('/health', (req, res) => {
     res.json({"status": "OK"});
 });
 
-app.listen(port, () => {
-    console.log(`Running on port: ${port}`)
+server.listen(port, () => {
+    console.log(`Running on port: ${port}`);
 });
 
 
