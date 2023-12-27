a9455234a2a3f2974f1438a5c0bda4be0c716d82
ArnavRupde <arnavrupade@gmail.com>
2023-12-27 15:58:17 +0530

    Add redis pub-sub

--- public/client.js
+++ public/client.js
@@ -1,23 +1,30 @@
 const webSocket = new WebSocket("ws://localhost:3000");
 
+let userId = null;
+
 function sendMessage() {
     const inputMsgText = document.getElementById("inputbox").value;
     webSocket.send(
         JSON.stringify({
             type: "message",
             payload: {
+                sender: userId,
                 message: inputMsgText
             }
         })
     );
-    const msgsElement = document.getElementById("allMessages");
-    msgsElement.innerHTML += `<h3 style="color:red;"> You: ` + inputMsgText + `</h3>`;
 }
 webSocket.onmessage = function (event) {
     console.log(`event: ${event.data}`);
     const msgData = JSON.parse(event.data);
-    if (msgData.type == 'message') {
+    if(msgData.type == 'joined') {
+        userId = msgData.payload.userId;
+    } else if (msgData.type == 'message') {
         const msgsElement = document.getElementById("allMessages");
-        msgsElement.innerHTML += `<h3 style="color:blue;"> Stranger: ` + msgData.payload.message + `</h3>`;
+        if (msgData.payload.sender == userId) {
+            msgsElement.innerHTML += `<h3 style="color:red;"> You: ` + msgData.payload.message + `</h3>`;
+        } else {
+            msgsElement.innerHTML += `<h3 style="color:blue;"> Stranger: ` + msgData.payload.message + `</h3>`;
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import http from 'http';
 import {WebSocketServer} from 'ws';
+import {createClient} from 'redis'
 import path from 'path'
+import {v4 as uuidv4} from 'uuid';
+require('dotenv').config()
 
 const app = express();
 const server = http.createServer(app);
@@ -11,18 +14,56 @@ app.use(express.static( path.resolve('./public') ));
 
 const port = 3000;
 
+// Set up redis publisher and subscriber client
+const redisConnectionUrl = process.env.REDIS_URL;
+
+const publisher = createClient({
+    url: redisConnectionUrl
+});
+const subscriber = createClient({
+    url: redisConnectionUrl
+});
+
+( async () => {
+    await publisher.connect();
+}) ();
+( async () => {
+    await subscriber.connect();
+}) ();
+
 wss.on("connection", async(ws, req)=> {
     console.log("client connected");
-    ws.on("message", (messageData) => {
+
+    // Assign random id to user
+    const userId = uuidv4();
+    ws.send(JSON.stringify({
+        type: 'joined',
+        payload: {
+            userId: userId
+        }
+    }));
+    ws.on("message", async(messageData) => {
         console.log(`Received message from client: ${messageData}`);
-        wss.clients.forEach(
-            function each(client) {
-                if(client !== ws)
-                    client.send(messageData.toString());
-            }
-        );
+        await publisher.publish('Chats', messageData.toString());
+        // wss.clients.forEach(
+        //     function each(client) {
+        //         if(client !== ws)
+        //             client.send(messageData.toString());
+        //     }
+        // );
     });
 });
+
+subscriber.subscribe('Chats', function(message, channel) {
+    if(channel == 'Chats') {
+        // Send message to all websocket clients
+        wss.clients.forEach(
+            function each(client) {
+                client.send(message);
+            }
+        )
+    }
+} );
 
 app.get('/health', (req, res) => {
     res.json({"status": "OK"});
